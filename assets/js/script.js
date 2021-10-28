041bf95a4f08bfa6a00ad1dfa0253c80bc70a96c
Anna Yujeong An <yujeong99an@gmail.com>
2021-10-27 23:49:49 -0700

    Added project images, implemented website (in progress)

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,11 +30,9 @@ function collapseNavBar() {
 
 function activateNavItem() {
     $(this).addClass("active");
-    $(this).css("color", "green");
 }
 
 function deActivateNavItem() {
-    $('.active').css("color", "black");
     $('.active').removeClass("active");
 }
 
@@ -43,13 +41,58 @@ function shadowNavWhenScrolling() {
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 }
 
-$(document).ready(function () {
+/** project overlay */
+function displayProjectDetails() {
+    var overlayElement = this.id + "-overlay";
+    document.getElementById(overlayElement).style.display = "block";
+
+    /** disable document body scrolling when the overlay elements are on 'on' */
+    document.body.style.overflow = 'hidden';
+}
+
+function hideProjectDetails() {
+    document.getElementById("green-space-overlay").style.display = "none";
+    document.getElementById("covaware-overlay").style.display = "none";
+
+    /** un-disable document body scrolling when the overlay elements are on 'on' */
+    document.body.style.overflow = 'scroll';
+}
+
+/** change color of nav bar element on scroll */
+function changeNavElementColor(id, start, end) {
+    $(window).scroll(() => {
+        var scroll = $(this).scrollTop();
+
+        if (scroll > start && scroll < end) {
+            $("#" + id).addClass("active");
+        } else {
+            $("#" + id).removeClass("active");
+        }
+        
+    });
+}
+
+function changeNavElementColorOnScroll() {
+    changeNavElementColor("nav-home", 0, 599);
+    changeNavElementColor("nav-about", 600, 1299);
+    changeNavElementColor("nav-education", 1300, 1799);
+    changeNavElementColor("nav-skills", 1800, 2299);
+    changeNavElementColor("nav-projects", 2300, 3199);
+    changeNavElementColor("nav-contacts", 3200, 4000);
+}
+
+$(document).ready(() => {
     $(document).scroll(shadowNavWhenScrolling);
-    $("#email-copy-button").on("click", copyEmailToClipBoard);
-    $("#linkedin-copy-button").on("click", copyLinkedinToClipBoard);
+    changeNavElementColorOnScroll();
 
     $(".nav-link").on('click', collapseNavBar);
     $(".nav-link").on('click', deActivateNavItem);
     $(".nav-link").on('click', activateNavItem);
+
+    $(".project-body").on("click", displayProjectDetails);
+    $(".overlay .fa-times").on("click", hideProjectDetails);
+
+    $("#email-copy-button").on("click", copyEmailToClipBoard);
+    $("#linkedin-copy-button").on("click", copyLinkedinToClipBoard);
 
 });
